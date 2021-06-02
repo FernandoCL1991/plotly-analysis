4218ced1215c0e38faa6a444074da4641efa36f2
FernandoCL <fernancors@hotmail.com>
2021-06-01 22:50:43 -0500

    Uploading current js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,94 +1,119 @@
-// Bar Chart for top 10 OTU's //
+// SELECTING USER INPUT
+var userInput = d3.select("#selDataset");
 
-// Helper function 
-// @param {array} rows
-// @param {integer} index
+// SELECTING BAR CHART TO CREATE IT
+var barChart = d3.select("#horizontal-bar-chart");
 
-// Unpack function
-function unpack(rows, index) {
-    return rows.map(function(row) {
-        return row[index];
-    });
+// INITIAL FUNCTION TO RUN WHEN BROWSER OPENS
+function init() {
+
+    // RESET DATA IF ANY
+    resetData();
+
+    // READING JSON
+    d3.json("samples").then((data => {
+
+        // LOOP THORUGH NAMES TO GIVE AS OPTIONS TO CHOOSE
+        data.names.forEach(name => {
+            var option = userInput.append("option");
+            option.text(name);
+        });
+
+        // CREATING VARIABLE WITH THE NAME CHOSEN
+        var initId = userInput.property("value")
+
+        // CREATING CHARTS WITH NAME CHOSEN
+        createChart(initId);
+
+    }));
 };
 
-// Data Promise
-const dataPromise = d3.json("samples.json");
-console.log("Data Promise: ", dataPromise);
+// RESET DATA FUNCTION TO RESET HTML
+function resetData() {
+    barChart.html("");
+};
 
+// FUNCTION TO GET DATA IN JSON AND START FORMATTING
+function createCharts() {
+    d3.json("samples.json").then((data) => {
+        console.log(data);
 
-sample_values = [];
-otu_ids = [];
-otu_labels = [];
+        // OTU_IDS
+        selectedId = data.samples.filter(subject => subject.id == id)[0];
 
-// Chart variables
-const samplesData = () => dataPromise = d3.json("samples.json");
-    console.log(dataPromise);
-    dataPromise.then(
-        (data) => {
+        // SAMPLE_VALUES
+        allSampleValues = data.samples.filter(sample => sample.id == id)[0];
+       
+        // EMPTY LISTS TO FILL WITH DATA ON EACH LOOP
+        var otu_ids = [];
+        var otu_labels = [];
+        var sample_values = [];
 
-            sample_values = [];
+        // ITERATION THROUGH DATA VIA SAMPLE AS KEY
+        Object.entries(allSampleValues).forEach(([key, value]) => {
 
-            // sample_values Listo!!!!
-            const sample_values = Object.samples.sample_values;
-            // var sample_values = data.samples.sample_values;
-            //console.log(sample_values);
-
-            // otu_ids Listo!!!!!
-            const otu_ids = Object.values(samples.otu_ids);
-            //console.log(otu_ids);
-
-            // otu_labels REVISAR....
-            // var otu_labels = data.samples
-            const otu_labels = Object.values(samples.otu_labels);
-            // unpack(data.samples,otu_labels,0)
-            //console.log(otu_labels);
-
-            const trace = [{
-                type: "bar",
-                orientation: "h",
-                name: otu_labels,
-                x: sample_values,
-                y: otu_ids,
-            }]
-
-            const layout = {
-                title: `TOP 10 OTU'S`,
-                xaxis: {
-                    range: [sample_values]
-                },
-                yaxis: {
-                    autorange: true,
-                    type: 'linear'
-                }
+            // CHANGING OBJECT VIA USER INPUT USING 'SWITCH' FUNCTION
+            switch (key) {
+                case "otu_ids":
+                    otu_ids.push(value);
+                    break;
+                case "otu_labels":
+                    otu_labels.push(value);
+                    break;
+                case "sample_values":
+                    sample_values.push(value);
+                    break;
+                default:
+                    break;
             }
-        Plotly.newPlot("plot", trace, layout)
-});
-samplesData();
+        }); // ITERATION AND RETRIEVEMENT OF DATA ENDS
 
+        // DISPLAYING TOP 10 RESULTS USING 'REVERSE' METHOD AND SLICE NOTATION
+        var top10otuIDs = otu_ids[0].slice(0, 10).reverse();
+        var top10otuLabels = otu_labels[0].slice(0, 10).reverse();
+        var top10sampleValues = sample_values[0].slice(0, 10).reverse();
 
+        var allInfoOTUids = top10otuIDs.map(otu_ids => "OTU " + otu_ids);
 
+        // TRACE FOR BAR CHART
+        var trace = {
+            x: top10sampleValues,
+            y: allInfoOTUids,
+            text: top10otuLabels,
+            type: "bar",
+            orientation: "h"
+        }
 
+        // TRACE AS DATA
+        var data = [trace];
 
+        // LAYOUT FOR PLOT
+        var layout = {
+            height: 450,
+            width: 550,
+            title: `TOP OTU's for ID: ${id}`,
+            xaxis: 'Sample Values',
+            yaxis: "OTU'S found"
+        };
 
+        // PLOTTING
+        plotly.newPlot("bar", data, layout);
+        ////>>>>>>>>>>> CONTINUAR EN MAP FUNCTION LINEA 124 DE LA PAGINA
+        //// https://github.com/neha-nayeem/plotly-challenge/blob/master/static/js/app.js
+        ///// https://tec.bootcampcontent.com/Tecnologico-de-Monterrey-Coding-Boot-Camp/tdm-mxc-data-pt-12-2020-u-c/-/tree/master/week-15-plotly/homework
+        
 
+})};
 
-// var data = [sampleValues];
+// OPTION CHANGED FUNCTION
+function optionChanged(id) {
+    
+    // WHEN OPTION IS CHANGED, GETS RE-SETED TO ORIGINAL
+    resetData();
 
-// let chart = d3.select("#horizontal-bar-chart").node();
-// Plotly.newPlot(chart, data);
+    // ONCE RESET IS DONE, CALL CREATE CHART FUNCTION
+    createCharts(id);
+}
 
-// // Creating trace
-// var trace1 = {
-//     x: samples.map(row => row.sample_values[0]),
-//     y: samples.map(row => row.otu_ids[0]),
-//     text: samples.map(row => row.otu_labels[0]),
-//     name: "OTU",
-//     type: "bar"
-// };
-// var traceData = [trace1];
-// // Creating layout
-// var layout = {
-//     title: "Top 10 OTU's",
-// };
-// // Creating plot
-// Plotly.newPlot("bar", traceData, layout);
+// CALLING INITIAL FUNCTION TO RUN WHEN BROWSER OPENS
+init();
