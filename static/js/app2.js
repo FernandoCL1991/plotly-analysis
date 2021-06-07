53a06a32911c7588b9140a83f1ff7c98bcd2a6ca
FernandoCL <fernancors@hotmail.com>
2021-06-07 11:52:01 -0500

    Uploading final updates

--- static/js/app2.js
+++ static/js/app2.js
@@ -1,5 +1,5 @@
 // FUNCTION TO GENERATE PLOTS
-function getPlots(id) {
+function getPlots(sample_id) {
 
     // READING JSON
     d3.json("../samples.json").then (sampledata =>{
@@ -7,40 +7,37 @@ function getPlots(id) {
 
         // OTU IDS
         var ids = sampledata.samples[0].otu_ids;
-        console.log(ids)
-
-        // SELECTING INPUT AS THE CHOSEN ID
-        var result1 = ids === id[0];
-
+        // console.log(ids)
+       
         // SELECTING HTML ELEMENT
         var barPlot = d3.select("#bar");
 
         // CLEARING OUT PREVIOUS QUERIES
         barPlot.html("");
 
-        // SELECTING INFO TO PLOT IN CHART
-        Object.entries(result1).forEach((key) => {
-            barPlot.text(key[0].toUpperCase());
-        });
+        // CREATING A VARIABLE TO THE SELECTED ID
+        var filteredData = sampledata.samples.filter (x => x.id === sample_id)[0];
+        // console.log(filteredData);
 
         // TOP 10 REVERSED SAMPLE VALUES
-        var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
-        console.log(sampleValues)
+        var sampleValues =  filteredData.sample_values.slice(0,10).reverse();
+        //console.log(sampleValues)
 
         // TOP 10 REVERSED VALUE LABELS
-        var labels =  sampledata.samples[0].otu_labels.slice(0,10);
-        console.log(labels)
+        var labels =  filteredData.otu_labels.slice(0,10);
+        //console.log(labels)
 
         // TOP 10 REVERSED OTU IDS
-        var OTU_top = (sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
-        
+        var OTU_top = (filteredData.otu_ids.slice(0, 10)).reverse();
+        // var console.log(OTU_TOP)
+
         // FORMAT OTU IDS FOR PLOT
         var OTU_id = OTU_top.map(d => "OTU " + d);
-        console.log(`OTU IDS: ${OTU_id}`)
+        //console.log(`OTU IDS: ${OTU_id}`)
 
         // PRINTING LABEL
-        var labels =  sampledata.samples[0].otu_labels.slice(0,10);
-        console.log(`OTU_labels: ${labels}`)
+        var labels =  filteredData.otu_labels.slice(0,10);
+        //console.log(`OTU_labels: ${labels}`)
         
         // CREATING TRACE FOR BAR CHART
         var trace1 = {
@@ -75,14 +72,14 @@ function getPlots(id) {
 
         // CREATING TRACE FOR BUBBLE CHART
         var trace2 = {
-            x: sampledata.samples[0].otu_ids,
-            y: sampledata.samples[0].sample_values,
+            x: filteredData.otu_ids,
+            y: filteredData.sample_values,
             mode: "markers",
             marker: {
-                size: sampledata.samples[0].sample_values,
-                color: sampledata.samples[0].otu_ids
+                size: filteredData.sample_values,
+                color: filteredData.otu_ids
             },
-            text:  sampledata.samples[0].otu_labels
+            text: filteredData.otu_labels
         };
 
         // LAYOUT FOR BUBBLE CHART
@@ -108,7 +105,7 @@ function getDemoInfo(id) {
 
         // SELECTING OUT METADATA
         var metadata = data.metadata;
-        console.log(metadata)
+        //console.log(metadata)
         
         // FILTERING DATA BY ID
         var result2 = metadata.filter(meta => meta.id.toString() === id)[0];
@@ -125,7 +122,7 @@ function getDemoInfo(id) {
         });
 
         // CALL GAUGE CHART
-        showGauge(result2.wfreq);
+        //showGauge(result2.wfreq);
     });
 }
 
@@ -137,21 +134,23 @@ function optionChanged(id) {
 
 // INITIAL FUNCTION
 function init() {
-    // select dropdown menu
+
+    // SELECT DROPDOWN MENU
     var dropdown = d3.select("#selDataset");
-    // read the data
+    
+    // READING DATA
     d3.json("../samples.json").then((data)=> {
-        console.log(data)
-        // get the id data to the dropdwown menu
+    // console.log(data)
+        
+        // GET ALL IDS INTO THE DROPDOWN MENU
         data.names.forEach(function(name) {
             dropdown.append("option").text(name).property("value");
         });
 
-// call the functions to display the data and the plots to the page
-
+// CALLING IN FUNCTIONS TO DISPLAY THE DATA
 getPlots(data.names[0]);
-    getDemoInfo(data.names[0]);
-    });
+getDemoInfo(data.names[0]);
+});
 };
 
 init();
